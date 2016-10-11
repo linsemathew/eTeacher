499365e8c16caa0429827b84c8398a33791a2b77
Linse <linsemathew123@gmail.com>
2016-10-10 22:03:51 -0400

    Finish homepage css.

--- routes/classes.js
+++ routes/classes.js
@@ -1,1 +1,15 @@
-classes.js
+var express = require('express');
+var router = express.Router();
+Class = require('../models/class')
+
+router.get('/', function(req, res, next) {
+	Class.getClasses(function(err, classes){
+		if (err){
+			res.send(error);
+		} else {
+			res.render('classes/index', {"classes": classes})
+		}
+	});
+});
+
+module.exports = router;
