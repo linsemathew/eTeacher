c6b0cdf1bed61e44a81f59388ab1557b6967ad79
Linse <linsemathew123@gmail.com>
2016-10-14 05:33:26 -0400

    Remove flash.

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var flash = require('connect-flash');
 
 var User = require('../models/user');
 var Student = require('../models/student');
